084a098db8d63c849c9734ce98668798e2f5661a
Beren Minor <beren.minor+git@gmail.com>
2017-06-16 23:39:39 +0200

    Force ${package} variable to the package that it actually requested

--- cereal-config-version.cmake
+++ cereal-config-version.cmake
@@ -1,4 +1,5 @@
 find_path(CEREAL_INCLUDE_DIR "cereal/cereal.hpp")
 
+set(package ${PACKAGE_FIND_NAME})
 add_library(${package}::${package} INTERFACE IMPORTED GLOBAL)
 set_target_properties(${package}::${package} PROPERTIES INTERFACE_INCLUDE_DIRS "${CEREAL_INCLUDE_DIR}")
