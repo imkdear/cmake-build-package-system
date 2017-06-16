4538486c7f353467d224b6f5bfbec8246bebf99a
Beren Minor <beren.minor+git@gmail.com>
2017-06-16 23:44:09 +0200

    Fail to detect cereal library if no headers are found

--- cereal-config-version.cmake
+++ cereal-config-version.cmake
@@ -1,5 +1,7 @@
-find_path(CEREAL_INCLUDE_DIR "cereal/cereal.hpp")
-
 set(package ${PACKAGE_FIND_NAME})
-add_library(${package}::${package} INTERFACE IMPORTED GLOBAL)
-set_target_properties(${package}::${package} PROPERTIES INTERFACE_INCLUDE_DIRS "${CEREAL_INCLUDE_DIR}")
+find_path(CEREAL_INCLUDE_DIR "cereal/cereal.hpp")
+if (CEREAL_INCLUDE_DIR)
+  add_library(${package}::${package} INTERFACE IMPORTED GLOBAL)
+  set_target_properties(${package}::${package} PROPERTIES INTERFACE_INCLUDE_DIRS "${CEREAL_INCLUDE_DIR}")
+  message(STATUS "Found ${package} (system)")
+endif()
